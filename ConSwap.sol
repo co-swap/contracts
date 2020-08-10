7cb36c4c8aafa2ce4c9f0a08d152d860b4d358c8
conswap <conswap@protonmail.com>
2020-08-10 11:41:48 +0800

    + contract Initializable

--- ConSwap.sol
+++ ConSwap.sol
@@ -1,5 +1,66 @@
 pragma solidity =0.5.16;
 
+/**
+ * @title Initializable
+ *
+ * @dev Helper contract to support initializer functions. To use it, replace
+ * the constructor with a function that has the `initializer` modifier.
+ * WARNING: Unlike constructors, initializer functions must be manually
+ * invoked. This applies both to deploying an Initializable contract, as well
+ * as extending an Initializable contract via inheritance.
+ * WARNING: When used with inheritance, manual care must be taken to not invoke
+ * a parent initializer twice, or ensure that all initializers are idempotent,
+ * because this is not dealt with automatically as with constructors.
+ */
+contract Initializable {
+
+  /**
+   * @dev Indicates that the contract has been initialized.
+   */
+  bool private initialized;
+
+  /**
+   * @dev Indicates that the contract is in the process of being initialized.
+   */
+  bool private initializing;
+
+  /**
+   * @dev Modifier to use in the initializer function of a contract.
+   */
+  modifier initializer() {
+    require(initializing || isConstructor() || !initialized, "Contract instance has already been initialized");
+
+    bool isTopLevelCall = !initializing;
+    if (isTopLevelCall) {
+      initializing = true;
+      initialized = true;
+    }
+
+    _;
+
+    if (isTopLevelCall) {
+      initializing = false;
+    }
+  }
+
+  /// @dev Returns true if and only if the function is running in the constructor
+  function isConstructor() private view returns (bool) {
+    // extcodesize checks the size of the code stored in an address, and
+    // address returns the current address. Since the code is still not
+    // deployed when running a constructor, any checks on its code size will
+    // yield zero, making it an effective way to detect if a contract is
+    // under construction or not.
+    address self = address(this);
+    uint256 cs;
+    assembly { cs := extcodesize(self) }
+    return cs == 0;
+  }
+
+  // Reserved storage space to allow for layout changes in the future.
+  uint256[50] private ______gap;
+}
+
+
 interface IUniswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
@@ -109,7 +170,7 @@ interface IUniswapV2Callee {
     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
 
-contract UniswapV2ERC20 is IUniswapV2ERC20 {
+contract UniswapV2ERC20 is IUniswapV2ERC20, Initializable {
     using SafeMath for uint;
 
     string public constant name = 'Uniswap V2';
@@ -128,6 +189,10 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     event Transfer(address indexed from, address indexed to, uint value);
 
     constructor() public {
+        initialize();
+    }
+    
+    function initialize() internal initializer {
         uint chainId;
         assembly {
             chainId := chainid
@@ -218,12 +283,21 @@ contract ConSwapPair is IUniswapV2Pair, UniswapV2ERC20 {
     uint public price1CumulativeLast;
     uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
 
-    uint private unlocked = 1;
+    //uint private unlocked = 1;
+    //modifier lock() {
+    //    require(unlocked == 1, 'UniswapV2: LOCKED');
+    //    unlocked = 0;
+    //    _;
+    //    unlocked = 1;
+    //}
+    // compatibility for Upgradeable, as described in
+    // https://docs.openzeppelin.com/upgrades/2.8/writing-upgradeable#avoid-initial-values-in-field-declarations
+    uint private locked;
     modifier lock() {
-        require(unlocked == 1, 'UniswapV2: LOCKED');
-        unlocked = 0;
+        require(locked == 0, 'ConSwap: LOCKED');
+        locked = 1;
         _;
-        unlocked = 1;
+        locked = 0;
     }
 
     function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
@@ -254,9 +328,9 @@ contract ConSwapPair is IUniswapV2Pair, UniswapV2ERC20 {
     }
 
     // called once by the factory at time of deployment
-    function initialize(address _token0, address _token1) external {
+    function initialize(address _token0, address _token1) external initializer {
         //require(msg.sender == factory, 'UniswapV2: FORBIDDEN'); // sufficient check
-        require(factory == address(0), 'ConSwapPair.initialize Can be delegatecall once by proxy only');
+        UniswapV2ERC20.initialize();
         factory = msg.sender;
         token0 = _token0;
         token1 = _token1;
